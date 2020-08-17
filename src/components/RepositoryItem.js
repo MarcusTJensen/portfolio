83f7b4f2c28ecb2378449cb0921351c33f08b1de
Marcus Jensen <marcusjensen@brokenMac.local>
2020-08-17 14:53:45 +0200

    link to repo not working yet

--- src/components/RepositoryItem.js
+++ src/components/RepositoryItem.js
@@ -25,23 +25,23 @@ const RepositoryItem = (props) => {
 
     return(
         <div id="itemContainer">
-            <div className="item" style={{
-                background: `url(https://cdn.jsdelivr.net/npm/programming-languages-logos/src/${languages[0]}/${languages[0]}.png)`
-                }}>
-                <div id="languagesDiv">
-                    {
-                        languages.map((language) => (
-                            <img className="languagesImg" src={
-                                language !== "shaderlab" ?
-                                `https://cdn.jsdelivr.net/npm/programming-languages-logos/src/${language}/${language}.png` :
-                                    'https://upload.wikimedia.org/wikipedia/commons/1/19/Unity_Technologies_logo.svg'
-                            } height="75" />
-                        ))
-                    }
+                <div onClick={() => window.location.href= `github.com/marcustjensen/${props.name}`} className="item" style={{
+                    background: `url(https://cdn.jsdelivr.net/npm/programming-languages-logos/src/${languages[0]}/${languages[0]}.png)`
+                    }}>
+                    <div id="languagesDiv">
+                        {
+                            languages.map((language) => (
+                                <img className="languagesImg" src={
+                                    language !== "shaderlab" ?
+                                    `https://cdn.jsdelivr.net/npm/programming-languages-logos/src/${language}/${language}.png` :
+                                        'https://upload.wikimedia.org/wikipedia/commons/1/19/Unity_Technologies_logo.svg'
+                                } height="75" />
+                            ))
+                        }
+                    </div>
+                    <p className="itemText">{props.name}</p>
+                    <p className="description">{props.description}</p>
                 </div>
-                <p className="itemText">{props.name}</p>
-                <p className="description">{props.description}</p>
-            </div>
         </div>
     );
 };
